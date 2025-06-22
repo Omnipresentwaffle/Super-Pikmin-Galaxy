7c4305ddf7e595f4680ea74745de0c0d95520383
Omnipresentwaffle <Josharoni.Pizza360@gmail.com>
2025-06-21 22:47:59 -0600

    Whistle Hitbox Expands

--- Scripts/Cursor.cs
+++ Scripts/Cursor.cs
@@ -5,17 +5,19 @@ public partial class Cursor : Area2D
 {
 	// Called when the node enters the scene tree for the first time.
 
-	public const double whistleTime = 3f;
-	public const double timeToFull = 1f;
+	public const float whistleTime = 2f;
+	public const float timeToFull = 0.5f;
 
 	public bool timeOut = false;
 
 	public const float minRad = 64f;
-	private const float maxRad = 128f;
+	private const float maxRad = 450f;
 
 	public float slope = 0f;
 
 	public double timer = 0f;
+
+	public bool whistlePress = false;
 	public bool whistling = false;
 
 	public Area2D whistleArea = null;
@@ -25,6 +27,7 @@ public partial class Cursor : Area2D
 		whistleArea = GetNode<Area2D>("WhistleHitbox");
 		whistleHbox = whistleArea.GetNode<CollisionShape2D>("CollisionShape2D");
 		slope = maxRad - minRad;
+		slope /= (float)timeToFull;
 
 
 	}
@@ -34,31 +37,54 @@ public partial class Cursor : Area2D
 	{
 		GlobalPosition = GetGlobalMousePosition();
 		whistleHandle(delta);
-	
+
 
 	}
 
-	public void whistleHandle(double delta) {
+	public void whistleHandle(double delta)
+	{
 		CircleShape2D shape = (CircleShape2D)whistleHbox.Shape;
-		if (!whistling)
+		if (!Input.IsActionPressed("whistle"))
 		{
-			timer = 0;
+			shape.Radius = minRad;
+			whistlePress = false;
+			timeOut = false;
+			return;
 		}
-		if (Input.IsActionPressed("whistle"))
+		if (timeOut)
 		{
-			timer += delta;
-			if (timer > whistleTime)
-			{
-				timeOut = true
-			}
-			if (timer >= timeToFull)
-			{
-				shape.Radius = maxRad;
-			}
-			else
-			{
-				shape.Radius = minRad + (slope * (float)timer);
-			}
+			return;
 		}
+		if (!whistlePress)
+		{
+			shape.Radius = minRad;
+			timer = 0f;
+			whistlePress = true;
+			
+		}
+
+		timer += delta;
+
+		if (timer > whistleTime)
+		{
+			//GD.Print("TIMEOUT");
+			timeOut = true;
+			shape.Radius = minRad;
+			timer = 0f;
+		}
+		else if (timer >= timeToFull)
+		{
+			shape.Radius = maxRad;
+		}
+		else
+		{
+			shape.Radius = (float)(minRad + (timer * slope));
+
+		}
+
+
+
+
 	}
+	
 }
